4caa5ae2f00243a321264cdfbdb9befa23549ee9
dmiramon <dmiramon@gmail.com>
2015-03-23 15:58:40 -0600

    Ejemplo de seed para poderse registrar
    Diego

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(first_name: "Dios", last_name: "Premier", email: "diospremier@xango.com", xango_id: "123456", iuvare_id: "1234", active: true, xango_rank: "DIOS", password:"xangoxango")
+user = User.create(first_name: "Dios", last_name: "Premier", email: "diospremier@xango.com", xango_id: "123456", iuvare_id: "1234", active: true, xango_rank: "DIOS", password:"xangoxango")
+
+#Invitation.create(user_id: user.id, recipient_email:"correo@correo.com", token:"cualquiercosa")
