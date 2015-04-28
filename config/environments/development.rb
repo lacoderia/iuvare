89c627a0212d35c5d18a4b82512032f02b0a976f
schultzRr <ZFja5Hdb>
2015-04-28 13:25:35 -0500

    Restore development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  ENV['HOST'] = "localhost:3000"
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -34,25 +37,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  ENV['ADDRESS'] = "smtp.gmail.com"
-  ENV['HOST'] = "localhost:3000"
-  ENV['DOMAIN_NAME'] = "coderia.mx"
-  ENV['MAIL_USERNAME'] = "ricardo@coderia.mx"
-  ENV['MAIL_PASSWORD'] = "ZFja5Hdb."
-
-  config.action_mailer.smtp_settings = {
-      address: ENV["ADDRESS"],
-      port: 587,
-      domain: ENV["DOMAIN_NAME"],
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["MAIL_USERNAME"],
-      password: ENV["MAIL_PASSWORD"]
-  }
-  # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => ENV["HOST"] }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
 end
