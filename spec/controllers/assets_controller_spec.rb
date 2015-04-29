41b9a80b25b00c376e3c45ad17d59dd978a164f8
tonklis <benjamin.hernandez@in2teck.com>
2015-04-29 13:38:23 -0500

    Added completed set of tests.

--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -1,14 +1,84 @@
 feature 'AssetsController' do
-  let!(:asset){ create(:asset, title: "Search for me") }
-  let!(:test){ create(:test, asset: asset) }
+  let!(:asset){ create(:asset, title: "Search Title") }
+  let!(:other_asset){ create(:asset) }
+  let!(:test){ create(:test, asset: asset, name: "Prueba test") }
 
   describe 'asset associations' do
 
-    it 'has correct associations' do
-      #visit "#{by_asset_type_assets_path}.json?asset_type=#{asset.asset_type}"
+    context 'by_asset_type' do
 
-      visit "#{by_keyword_and_asset_type_assets_path}.json?asset_type=#{asset.asset_type}&keyword=#{'search'}"
-      response = JSON.parse(page.body)
+      it 'has correct associations' do
+        visit "#{by_asset_type_assets_path}.json?asset_type=#{asset.asset_type}"
+
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+
+        assets = response['result']['assets']
+        expect(assets.count).to eql 2
+        expect(assets.first['id']).to eql asset.id
+        expect(assets.first['test']['name']).to eql "Prueba test"
+        expect(assets.last['id']).to eql other_asset.id
+        expect(assets.last['test']).to be nil
+      end
+      
+      it 'responds with empty resultset' do
+        visit "#{by_asset_type_assets_path}.json?asset_type=audio"
+        
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+        expect(response['result']).to be nil
+      end
+
+    end
+
+    context 'by_keyword_and_asset_type' do
+
+      let!(:asset_description){ create(:asset, description: "Search DESCRIPTION") }
+      let!(:asset_author){ create(:asset, author: "Search author") }
+    
+      it 'has correct associations' do
+        visit "#{by_keyword_and_asset_type_assets_path}.json?asset_type=#{asset.asset_type}&keyword=#{'TITLE'}"
+
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+        assets = response['result']['assets']
+        expect(assets.count).to eql 1
+        expect(assets.first['id']).to eql asset.id
+        expect(assets.first['test']['name']).to eql "Prueba test"
+
+        visit "#{by_keyword_and_asset_type_assets_path}.json?asset_type=#{asset.asset_type}&keyword=#{'description'}"
+
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+        assets = response['result']['assets']
+        expect(assets.count).to eql 1
+        expect(assets.first['id']).to eql asset_description.id
+
+        visit "#{by_keyword_and_asset_type_assets_path}.json?asset_type=#{asset.asset_type}&keyword=#{'Author'}"
+
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+        assets = response['result']['assets']
+        expect(assets.count).to eql 1
+        expect(assets.first['id']).to eql asset_author.id
+
+        visit "#{by_keyword_and_asset_type_assets_path}.json?asset_type=#{asset.asset_type}&keyword=#{'search'}"
+
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+        assets = response['result']['assets']
+        expect(assets.count).to eql 3
+
+      end
+
+      it 'responds with empty resultset' do
+        visit "#{by_asset_type_assets_path}.json?asset_type=audio&keyword=#{'audio'}"
+        
+        response = JSON.parse(page.body)
+        expect(response['success']).to be true
+        expect(response['result']).to be nil
+      end
+
     end
     
   end
--- spec/factories/assets_factory.rb
+++ spec/factories/assets_factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :asset, class: Asset do
     title "Training for test asset"
-    description "Description of training where it happened"
-    author "Author Name"
-    source "source_name.vid"
+    description "Where training happened"
+    author "Bejoview Heinkudez"
+    source "source_name"
     purchasable false
     price 0.0
     asset_type "training"
