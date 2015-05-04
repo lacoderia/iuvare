f5660ef35a1f323fb491fc09e178e5aa3ee54d3b
Luis Antonio Sánchez <luis.sanchez.franco@gmail.com>
2015-05-04 16:44:47 -0500

    H8-T1: Front de Biblioteca
    - Se agregaron los controladores de los assets y se integraron los servicios
    
    #32993961196192

--- app/assets/javascripts/controllers/SeminarController.js
+++ app/assets/javascripts/controllers/SeminarController.js
@@ -1,6 +1,35 @@
 'use strict';
 
-iuvare.controller('SeminarController', ["$scope", "$rootScope", "AuthService", "SessionService", "DEFAULT_VALUES", function($scope, $rootScope, AuthService, SessionService, DEFAULT_VALUES){
+iuvare.controller('SeminarController', ["$scope", "$rootScope", "AssetService", "AuthService", "SessionService", "DEFAULT_VALUES", function($scope, $rootScope, AssetService, AuthService, SessionService, DEFAULT_VALUES){
+
+    // Private variables
+    var ASSET_TYPE = 'seminar';
+    var playlable = true;
+
+    // Public variables
+    $scope.assetList = [];
+    $scope.assetQuery = undefined;
+
+    $scope.areAssetsAvailable = function(){
+        return ($scope.assetList.length)? true : false;
+    };
+
+    // Method that toggles a goal's information form
+    $scope.toggleAssetInfo = function(assetItem){
+
+        angular.forEach($scope.assetList, function(asset){
+            if(asset.id != assetItem.id){
+                asset.showInfo = false;
+            }
+        });
+
+        (assetItem.showInfo)? assetItem.showInfo = false : assetItem.showInfo = true;
+
+    };
+
+    $scope.isPlaylable = function () {
+        return playlable;
+    };
 
     // Method to init the controller's default state
     $scope.initController = function(){
@@ -9,7 +38,15 @@ iuvare.controller('SeminarController', ["$scope", "$rootScope", "AuthService", "
 
         $scope.sectionTitle = $scope.currentSubsection.title;
 
-
+        AssetService.getAssetsByType(ASSET_TYPE)
+            .success(function(data){
+                if(data.success){
+                    $scope.assetList = AssetService.assets;
+                }
+            })
+            .error(function (error, status) {
+                console.log('Hubo un error al obtener los seminarios.');
+            });
     };
 
     $scope.initController();
