dd2205db76cf3fd62d9d4ab861730013e9b5b714
schultzRr <ZFja5Hdb>
2015-05-12 15:39:26 -0500

    H7-T1 Perfil de usuario (frontend)
    La forma del perfil acepta cambios de foto de perfil y es validada antes de llamar al servicio de actualización
    
    #33847423682054

--- app/assets/javascripts/controllers/ProfileController.js
+++ app/assets/javascripts/controllers/ProfileController.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-iuvare.controller('ProfileController', ["$scope", "$rootScope", "AuthService", "TestService", "SessionService", "DEFAULT_VALUES", function($scope, $rootScope, AuthService, TestService, SessionService, DEFAULT_VALUES){
+iuvare.controller('ProfileController', ["$scope", "$rootScope", "AuthService", "TestService", "SessionService", "UserService", "DEFAULT_VALUES", function($scope, $rootScope, AuthService, TestService, SessionService, UserService, DEFAULT_VALUES){
 
     $scope.TEST_CODES = DEFAULT_VALUES.TEST_CODES;
 
@@ -88,6 +88,72 @@ iuvare.controller('ProfileController', ["$scope", "$rootScope", "AuthService", "
         }
     };
 
+
+
+    // PROFILE
+
+    $scope.currentUser = {
+        name: undefined,
+        lastName: undefined,
+        email: undefined,
+        iuvareId: undefined,
+        xangoId: undefined
+    };
+
+    $scope.openProfilePictureSelector = function(event) {
+        $(event.target).siblings('input').trigger('click');
+        return false;
+    };
+
+    $scope.selectProfilePicture = function(element) {
+        var input = $(element);
+        if (input[0].files && input[0].files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                var imageContainer = $(element).parents('.profile-form').find('.picture');
+                var image = imageContainer.find('img');
+                image.attr('src', e.target.result);
+                $scope.currentUser.picture = e.target.result;
+
+                var loadedImage = new Image();
+                loadedImage.src = reader.result;
+
+                var ratio = loadedImage.width / loadedImage.height;
+
+                // Si la imagen es horizontal, el alto debe ser el del contenedor y el ancho debe ser proporcional
+                if (loadedImage.width > loadedImage.height) {
+                    image.height(imageContainer.height());
+                    image.width(imageContainer.height() * ratio);
+                } else {
+                    // Si la imagen es vertical o cuadrada, el ancho debe ser el del contenedor y el alto debe ser proporcional
+                    image.width(imageContainer.width());
+                    image.height(imageContainer.width() / ratio);
+                }
+
+            };
+
+            reader.readAsDataURL(input[0].files[0]);
+        }
+    };
+
+    $scope.updateProfile = function() {
+        if ($scope.profileForm.$valid) {
+
+            var user = $scope.currentUser;
+
+            ProfileService.submitWeekCalendar(weekCalendar)
+                .success(function (data) {
+
+                })
+                .error(function (response) {
+
+                });
+
+        }
+    };
+
+
     // Method to init the controller's default state
     $scope.initController = function(){
         $scope.$emit('setCurrentSection');
@@ -107,6 +173,14 @@ iuvare.controller('ProfileController', ["$scope", "$rootScope", "AuthService", "
             .error(function (error, status) {
                 console.log('Hubo un error al obtener los resultados del test de color.');
             });
+
+        $scope.currentUser = {
+            name: SessionService.$get().getFirstName(),
+            lastName: SessionService.$get().getLastName(),
+            email: SessionService.$get().getEmail(),
+            iuvareId: SessionService.$get().getIuvareId(),
+            xangoId: SessionService.$get().getXangoId()
+        };
 
     };
 
