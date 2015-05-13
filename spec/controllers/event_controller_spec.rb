789d2187e8a70b8d1c716b360b3e96a5b8e90f2e
AlbertoSimonin <alberto.simonin@in2teck.com>
2015-05-12 19:17:24 -0500

    remover bloque timecop en event tests

--- spec/controllers/event_controller_spec.rb
+++ spec/controllers/event_controller_spec.rb
@@ -9,37 +9,34 @@ feature 'EventsController' do
 
 			it 'should get past event' do
 			
-			  Timecop.travel(Time.zone.local(2015, 4, 25, 13, 30, 0)) do
+			  Timecop.travel(Time.zone.local(2015, 4, 25, 13, 30, 0))
           visit "#{current_events_path}.json"
 
           response = JSON.parse(page.body)
           expect(response['success']).to be true
           expect(response['result'][0]['id']).to eql 1
-        end
 
 			end
 			
 			it 'should get current event' do
 			
-			  Timecop.travel(Time.zone.local(2015, 5, 12, 13, 30, 0)) do
+			  Timecop.travel(Time.zone.local(2015, 5, 12, 13, 30, 0))
           visit "#{current_events_path}.json"
 
           response = JSON.parse(page.body)
           expect(response['success']).to be true
           expect(response['result'][0]['id']).to eql 5
-        end
 
 			end
 			
 			it 'should get next event' do
 			
-			  Timecop.travel(Time.zone.local(2015, 6, 5, 13, 30, 0)) do
+			  Timecop.travel(Time.zone.local(2015, 6, 5, 13, 30, 0))
           visit "#{current_events_path}.json"
 
           response = JSON.parse(page.body)
           expect(response['success']).to be true
           expect(response['result'][0]['id']).to eql 9
-        end
 
 			end
 
