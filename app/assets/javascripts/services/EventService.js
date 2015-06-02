bda161dd4090c793048423f9003083426914f51c
schultzRr <ZFja5Hdb>
2015-06-02 18:29:49 -0500

    Hot-fix: Si no hay un evento en el mes no causa un error en consola

--- app/assets/javascripts/services/EventService.js
+++ app/assets/javascripts/services/EventService.js
@@ -14,7 +14,7 @@ iuvare.factory('EventService', ['$http', '$q', 'DEFAULT_VALUES', function($http,
 
         return $http.get(eventServiceURL)
             .success(function(data){
-                if(data.success && data.result){
+                if(data.success && data.result && data.result.length){
                     service.event = data.result[0];
                 }
             });
