a2eeb2fe6cb712d260f3f6fb6857923fe8958dc5
schultzRr <ZFja5Hdb>
2015-07-08 12:06:20 -0500

    Bug fix del timer del video. Ahora ya se despliegan correctamente los videos de más de una hora de duración.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -583,3 +583,20 @@ iuvare.filter('trustAsResourceUrl', function($sce) {
         return $sce.trustAsResourceUrl(val);
     };
 });
+
+iuvare.filter('millSecondsToTimeString', function() {
+    return function(s) {
+        function addZ(n) {
+            return (n<10? '0':'') + n;
+        }
+
+        var ms = s % 1000;
+        s = (s - ms) / 1000;
+        var secs = s % 60;
+        s = (s - secs) / 60;
+        var mins = s % 60;
+        var hrs = (s - mins) / 60;
+
+        return (hrs ? addZ(hrs) + ':' : '' ) + addZ(mins) + ':' + addZ(secs);
+    }
+});
