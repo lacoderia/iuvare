20bc70eea65fa8bc65128df17ad083da8add75ca
tonklis <benjamin.hernandez@in2teck.com>
2015-07-23 13:43:55 -0500

    Added order by last updated on assets.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -15,15 +15,13 @@ class Asset < ActiveRecord::Base
 
   validates :asset_type, inclusion: {in: TYPES.map{ |pairs| pairs[0] } }
 
-  scope :by_asset_type, -> (asset_type){where(asset_type: asset_type)}
-  scope :by_keyword, -> (keyword){where("lower(title) like '%#{keyword}%' OR lower(description) like '%#{keyword}%' OR lower(author) like '%#{keyword}%'")}
+  scope :by_asset_type, -> (asset_type){where(asset_type: asset_type).order(updated_at: :desc)}
+  scope :by_keyword, -> (keyword){where("lower(title) like '%#{keyword}%' OR lower(description) like '%#{keyword}%' OR lower(author) like '%#{keyword}%'").(updated_at: :desc)}
 
   def self.stream_file asset_type, source
 
     filepath = "#{ENV["PATH_TO_ASSETS"]}/#{asset_type}s/" + source
-
     video_extension = File.extname(filepath)[1..-1]
-
     return [filepath, video_extension]
     
   end
