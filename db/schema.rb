9447cb94c74e4033b4504d0d836441bad4d79835
tonklis <benjamin.hernandez@in2teck.com>
2015-08-28 19:00:09 -0500

    Unaccent enabled for postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150824221731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "unaccent"
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
