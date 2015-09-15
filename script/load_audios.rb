f22db2594427f7a3dd686bf817770e37f808e98b
tonklis <benjamin.hernandez@in2teck.com>
2015-09-15 14:48:02 -0500

    Added corrected scripts for historic_assets

--- script/load_audios.rb
+++ script/load_audios.rb
@@ -2,5 +2,5 @@
 require_relative "../config/environment"
 
 CSV.foreach(File.path("audios.csv"), { :col_sep => '|' }) do |col|
-  #puts col[0]
+  HistoricAsset.create(historic_asset_type: "audio", number: col[0], name: col[1], author: col[2], color: "purple")
 end
--- script/load_books.rb
+++ script/load_books.rb
@@ -2,5 +2,5 @@
 require_relative "../config/environment"
 
 CSV.foreach(File.path("books.csv"), { :col_sep => '|' }) do |col|
-  #puts col[0]
+  HistoricAsset.create(historic_asset_type: "book", number: col[0], name: col[1], author: col[2])
 end
