eda86a3b495473915496211871e970efbd0740e7
tonklis <benjamin.hernandez@in2teck.com>
2017-08-21 23:18:27 -0500

    Added counters

--- script/cancel_users.rb
+++ script/cancel_users.rb
@@ -1,11 +1,20 @@
 #!/usr/bin/env ruby
 require_relative "../config/environment"
 
+inactive = 0
+not_found = 0
+
 CSV.foreach(File.path("cancelled_users_gdl.txt"), { :col_sep => '|' }) do |col|
   iuvare_id = col[0]
-  user = User.find_by_iuvare_id("#{iuvare_Id}")
+  user = User.find_by_iuvare_id("#{iuvare_id}")
   if user
     user.active = false
     user.save
+    inactive += 1
+    puts "#{iuvare_id}"
+  else
+    not_found += 1
   end
 end
+
+puts "#{inactive} usuarios desactivados, #{not_found} usuarios no encontrados"
